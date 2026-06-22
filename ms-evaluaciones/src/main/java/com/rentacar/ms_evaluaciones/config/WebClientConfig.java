059ca7afcf551422d1d2f19db7009daa739027f8
TestDEV_CL <152047444+TestDEV-CL@users.noreply.github.com>
2026-06-22 15:25:13 -0400

    Added webclient

---
+++ ms-evaluaciones/src/main/java/com/rentacar/ms_evaluaciones/config/WebClientConfig.java
@@ -0,0 +1,27 @@
+package com.rentacar.ms_evaluaciones.config;
+
+import java.util.List;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(new Server().url("/").description("Ruta Relativa Centralizada")));
+    }
+}
